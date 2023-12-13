5bfe23cada16072703281aedc9e93abc27d66e70
Tommi Muona <43796235+muonato@users.noreply.github.com>
2023-12-13 13:28:22 +0200

    Update check_cert.sh to accept revocation list checks

--- check_cert.sh
+++ check_cert.sh
@@ -2,31 +2,34 @@
 #
 # muonato/check_cert.sh @ GitHub (13-DEC-2023)
 #
-# Reports ssl certificate(s) expiration date using openssl,
+# Reports ssl certificate(s) revocation list or expiration date,
 # compatible with Nagios monitoring as host plugin
 #
 # Usage:
-#       bash check_cert.sh <path-to-cert> [<path-to-cert>] ...
+#       bash check_cert.sh [OPTIONS] <path-to-cert> [<path-to-cert>] ...
 #
 #       Nagios nrpe configuration on host :
 #       command[check_cert]=/path/to/plugins/check_cert.sh $ARG1$
 #
-# Parameters:
-#       1: Path to ssl certificate file
+# Options:
+#       -r  revocation list date
 #
 # Examples:
 #       $ bash check_cert.sh /path/to/cert.pem
-#       (single certificate on host)
+#       (certificate expiry date)
+#
+#       $ bash chech_cert.sh -r /path/to/cert.crl.pem
+#       (certificate revocation list date)
 #
 #       check_nrpe -H $HOSTADDRESS$ -c check_cert -a '/path/to/cert.pem /path/to/ssl.crt'
 #       (nagios monitor expression for two certificates)
 #
-function cert_expiry () {
+function cert_query () {
     # Certificate path as function argument
     CPATH=$1
 
-    # Get certificate expiration date ignoring errors
-    CDATE=$(openssl x509 -noout -enddate -in $CPATH 2>/dev/null|grep -Po '=\K[^"]*')
+    # Get certificate due date - ignoring errors
+    CDATE=$(openssl $PARAM -noout -in $CPATH 2>/dev/null|grep -Po '=\K[^"]*')
 
     # Show as full date; same as %Y-%m-%d
     CSHOW=$(date -d "$CDATE" "+%F")
@@ -37,36 +40,52 @@ function cert_expiry () {
     # Today as unix epoch
     TODAY=$(date '+%s')
 
-    # Count certificate valid days left
+    # Count days left to due date
     CLEFT=$(( ( CEXPR - TODAY )/(60*60*24) ))
 
     # Return status message
     if [[ -z "$CDATE" ]]; then
-        echo "UNKNOWN - Certificate '$CPATH' openssl failed"
+        echo "UNKNOWN - Certificate '$CPATH' $CFUNC"
     elif [[ $CLEFT -ge 30 ]]; then
-        echo "OK - Certificate '$CPATH' expires '$CSHOW' (in $(( $CLEFT / 30 )) months)"
+        echo "OK - Certificate '$CPATH' $CFUNC '$CSHOW' (in $(( $CLEFT / 30 )) months)"
     elif [[ $CLEFT -ge 15 ]]; then
-        echo "WARNING - Certificate '$CPATH' expires '$CSHOW' (in $CLEFT days)"
+        echo "WARNING - Certificate '$CPATH' $CFUNC '$CSHOW' (in $CLEFT days)"
     elif [[ $CLEFT -ge 0 ]]; then
-        echo "CRITICAL - Certificate '$CPATH' expires '$CSHOW' (in $CLEFT days)"
+        echo "CRITICAL - Certificate '$CPATH' $CFUNC '$CSHOW' (in $CLEFT days)"
     else
-        echo "CRITICAL - Certificate '$CPATH' is invalid"
+        echo "CRITICAL - Certificate '$CPATH' $CFUNC is due"
     fi
 }
 
 # BEGIN __main__
-if [[ -z "$1" ]]; then
-    echo -e "check ssl certificate expiry\n\tUsage:\
-    `basename $0` </path/to/cert> [</path/to/cert>...</path/to/cert]>]\n
+USAGE="check ssl certificate\n\tUsage:\
+    `basename $0` [-r] </path/to/cert> [</path/to/cert> ...>]\n
     \tERROR: missing path to certificate"
+
+# Validate arguments
+if [[ -z "$1" ]]; then
+    echo -e $USAGE
     exit 3
+elif [[ $1 == "-r" ]]; then
+    PARAM="crl -nextupdate"
+    CFUNC="revocation"
+    INDEX=2
 else
-     CSTAT=""
+    PARAM="x509 -enddate"
+    CFUNC="expiry"
+    INDEX=1
 fi
 
-# Loop args for status message
-for (( i=1; i<=$#; i++ )); do
-    CSTAT="${CSTAT}${i}: $(cert_expiry ${@:i:1})\n"
+# Status message
+CSTAT=""
+
+# Status ordinal
+CNUMR=1
+
+# Loop args to append status message
+for (( i=$INDEX; i<=$#; i++ )); do
+    CSTAT="${CSTAT}${CNUMR}: $(cert_query ${@:i:1})\n"
+    ((CNUMR++))
 done
 
 # Print status excl. newline
@@ -79,6 +98,9 @@ elif [[ -n $(echo -e $CSTAT|grep -om 1 "WARNING") ]]; then
     exit 1
 elif [[ -n $(echo -e $CSTAT|grep -om 1 "UNKNOWN") ]]; then
     exit 3
-else
+elif [[ -n $(echo -e $CSTAT|grep -om 1 "OK") ]]; then
     exit 0
+else
+    echo -e $USAGE
+    exit 3
 fi
