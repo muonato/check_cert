e0d40179948547d2936e2b156a6d980ddbdffe17
Tommi Muona <43796235+muonato@users.noreply.github.com>
2025-12-03 07:29:11 +0200

    Certificate reporting script

--- check_cert.sh
+++ check_cert.sh
@@ -1,38 +1,53 @@
 #!/usr/bin/env bash
 #
-# muonato/check_cert.sh @ GitHub (24-JUL-2024)
+# muonato/check_cert.sh @ GitHub (02-DEC-2025)
 #
-# Reports SSL certificate(s) expiration date or revocation,
-# compatible with Nagios monitoring as host plugin
+# Reports SSL certificate(s) expiration date or revocation
 #
 # Usage:
-#       bash check_cert.sh </path/to/file> [</path/to/file>] ...
+#       bash check_cert.sh -f '</path/to/file> [</path/to/file>]'
 #
 #       Nagios nrpe configuration on host :
 #       command[check_cert]=/path/to/plugins/check_cert.sh $ARG1$
 #
+# Arguments:
+#       -f : String of certificate paths
+#
 # Options:
-#       None
+#       -h : Display help
+#       -w : Warning alert threshold in days, default: 60
+#       -c : Critical alert threshold in days, default: 30
 #
 # Examples:
-#       $ bash check_cert.sh /path/to/cert.pem
-#       (Certificate expiry or revocation list date)
+#       1. Expiry warning alert when 90 days remain
 #
-#       check_nrpe -H $HOSTADDRESS$ -c check_cert -a '/path/to/cert.pem /path/to/ssl.crt'
-#       (Nagios monitor expression for two certificates)
+#           $ bash check_cert.sh -f "/path/to/cert.pem" -w 90
 #
-# Notes:
-#       GNU bash, version 4.2.46(2)-release (x86_64-redhat-linux-gnu)
-#       CentOS Linux release 7.9.2009 (Core)
-#       OpenSSL 1.0.2k-fips 26 Jan 2017
-#       Opsview Core 3.20140409.0
+#       2. Nagios monitoring expression for two certificates
 #
+#           check_nrpe -H $HOSTADDRESS$ -c check_cert \
+#               -a '-f "/path/to/cert.pem /path/to/ssl.crt" -w 90'
+#
+
+function help() {
+        cat <<EOF
+Usage: $(basename "$0") -f "</path/to/cert> ... [/path/to/cert]"> [OPTIONS]
+
+OPTIONS
+    -f string of file paths
+    -h display this help menu
+    -w warning alert in days
+    -c critical alert in days
+
+EOF
+}
+
 function cert_query () {
     # Certificate path as function argument
     CPATH=$1
 
     # Inspect CRL occurrence on first line
-    ISCRL=$(sed -n '1p' $CPATH|grep -o "CRL")
+    ISCRL=$(sed -n '1p' $CPATH 2>/dev/null|grep -o "CRL")
 
     if [[ -z "$ISCRL" ]]; then
         PARAM="x509 -enddate"
@@ -42,7 +57,7 @@ function cert_query () {
         CFUNC="revocation"
     fi
 
-    # Get the due date - suppress errors
+    # Get the status - suppress error messages
     CDATE=$(openssl $PARAM -noout -in $CPATH 2>/dev/null|grep -Po '=\K[^"]*')
 
     # Show as full date; same as %Y-%m-%d
@@ -60,25 +75,38 @@ function cert_query () {
     # Return status message
     if [[ -z "$CDATE" ]]; then
         echo "UNKNOWN - Certificate '$CPATH' $CFUNC"
-    elif [[ $CLEFT -ge 30 ]]; then
+    elif [[ $CLEFT -ge $WARN ]]; then
         echo "OK - Certificate '$CPATH' $CFUNC '$CSHOW' (in $(( $CLEFT / 30 )) months)"
-    elif [[ $CLEFT -ge 15 ]]; then
+    elif [[ $CLEFT -ge $CRIT ]]; then
         echo "WARNING - Certificate '$CPATH' $CFUNC '$CSHOW' (in $CLEFT days)"
     elif [[ $CLEFT -ge 0 ]]; then
         echo "CRITICAL - Certificate '$CPATH' $CFUNC '$CSHOW' (in $CLEFT days)"
     else
-        echo "CRITICAL - Certificate '$CPATH' $CFUNC is due"
+        echo "CRITICAL - Certificate '$CPATH' $CFUNC has expired"
     fi
 }
 
 # BEGIN __main__
-USAGE="check ssl certificate\n\tUsage:\
-    `basename $0` </path/to/cert> [</path/to/cert> ...>]\n"
 
-# Validate arguments
-if [[ -z "$1" ]]; then
-    echo -e $USAGE
-    exit 3
+# Alerts
+WARN="60"
+CRIT="30"
+
+# Script arguments validation
+while getopts "hf:w:c:" opt; do
+    case "$opt" in
+        h)  help; exit 0;;
+        f)  CERT=$OPTARG;;
+        w)  WARN=$OPTARG;;
+        c)  CRIT=$OPTARG;;
+    esac
+done
+
+# Path to file required
+if [[ -z $CERT ]]; then
+    echo "$0: certificate path missing"
+    help
+    exit 0
 fi
 
 # Status message
@@ -87,16 +115,16 @@ CSTAT=""
 # Status ordinal
 CNUMR=1
 
-# Loop args to append status message
-for (( i=1; i<=$#; i++ )); do
-    CSTAT="${CSTAT}${CNUMR}: $(cert_query ${@:i:1})\n"
+# Loop to append status message
+for cer in ${CERT[@]}; do
+    CSTAT="${CSTAT}${CNUMR}: $(cert_query ${cer})\n"
     ((CNUMR++))
 done
 
 # Print status excl. newline
 echo -e ${CSTAT%??}
 
-# Apply exit code corresponding to status message
+# Exit with code corresponding to status message
 if [[ -n $(echo -e $CSTAT|grep -om 1 "CRITICAL") ]]; then
     exit 2
 elif [[ -n $(echo -e $CSTAT|grep -om 1 "WARNING") ]]; then
